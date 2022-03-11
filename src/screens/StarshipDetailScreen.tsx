86dd516e80aa0eb38aec6656a2901ab5732cdc5b
nicolas <berqueznicolas@gmail.com>
2022-03-11 18:47:06 +0100

    Final Publish

--- src/screens/StarshipDetailScreen.tsx
+++ src/screens/StarshipDetailScreen.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
+  ImageBackground,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -46,65 +47,95 @@ export const StarshipDetailScreen = (props: {
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
-        <Image style={styles.tinyLogo} source={imageMap(x)} />
-        <Text style={styles.model}>{item.name}</Text>
-        <Text style={styles.manufacturer}>{item.manufacturer}</Text>
-        <Text style={styles.iconText}>
-          ⭐ Rating :{item.hyperdrive_rating}/5.0
-        </Text>
-        <Text style={styles.iconText2}>
-          🚀 Max Speed :{item.max_atmosphering_speed}
-        </Text>
-        <Text style={styles.simpleText}>👨‍🚀 Crew: {item.crew}</Text>
-        <Text style={styles.simpleText}>💺 Passengers: {item.passengers}</Text>
-        <Text style={styles.simpleText}>
-          🍪 Consumables: {item.consumables}
-        </Text>
-        <Text style={styles.simpleText}>
-          🧳 cargo_capacity: {item.cargo_capacity}
-        </Text>
+        <ImageBackground
+          resizeMode={"cover"}
+          source={require("../../assets/DetailsScreen.jpg")}
+          style={styles.backgroundImage}
+        >
+          <Image style={styles.tinyLogo} source={imageMap(x)} />
+          <Text style={styles.model}>{item.name}</Text>
+          <Text style={styles.manufacturer}>{item.manufacturer}</Text>
+          <Text style={styles.iconText}>
+            ⭐ Rating :{item.hyperdrive_rating}/5.0
+          </Text>
+          <Text style={styles.iconText2}>
+            🚀 Max Speed :{item.max_atmosphering_speed}
+          </Text>
+          <Text style={styles.simpleText}>👨‍🚀 Crew: {item.crew}</Text>
+          <Text style={styles.simpleText}>
+            💺 Passengers: {item.passengers}
+          </Text>
+          <Text style={styles.simpleText}>
+            🍪 Consumables: {item.consumables}
+          </Text>
+          <Text style={styles.simpleText}>
+            🧳 cargo_capacity: {item.cargo_capacity}
+          </Text>
+        </ImageBackground>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+  },
+  container: {},
   tinyLogo: {
     width: "80%",
     height: 250,
     alignSelf: "center",
   },
   model: {
-    color: "black",
     fontSize: 25,
     fontWeight: "bold",
     textAlign: "left",
     marginLeft: 40,
+    color: "#2D69DC",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+    textShadowColor: "#2D69DC",
   },
   manufacturer: {
-    color: "black",
     fontSize: 18,
     textAlign: "left",
     marginLeft: 40,
+    color: "#2D69DC",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+    textShadowColor: "#2D69DC",
   },
   iconText: {
-    color: "black",
+    color: "white",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+    textShadowColor: "white",
     fontSize: 25,
     textAlign: "left",
     marginLeft: 40,
     marginTop: 40,
   },
   iconText2: {
-    color: "black",
+    color: "white",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+    textShadowColor: "white",
     fontSize: 25,
     textAlign: "left",
     marginLeft: 40,
     marginBottom: 40,
   },
   simpleText: {
-    color: "black",
+    color: "white",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 10,
+    textShadowColor: "white",
     fontSize: 25,
     textAlign: "left",
     marginLeft: 40,
+  },
+  backgroundImage: {
+    height: "100%",
   },
 });
